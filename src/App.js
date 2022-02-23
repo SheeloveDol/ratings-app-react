4e9954524c21160a80eb967a7dd1dfb931c13df1
Sheelove Dol <siesel26@gmail.com>
2022-02-22 20:18:46 -0800

    Added uuidv4 via npm and finished feedback submission

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import {useState} from 'react'
@@ -6,28 +7,38 @@ import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 
 function App() {
-  const [feedbacks, setFeedbacks] = useState(FeedbackData);
+    const [feedbacks, setFeedbacks] = useState(FeedbackData);
 
-  // function to delete feedback/ratings. We'll have to 'prop-drill' all the way to FeedbackItem
-  const deleteFeedback = (id) => {
-    // Creating a warning popup to confirm delete action
-    if (window.confirm('Do you want to delete this feedback forever?')) {
-      setFeedbacks(feedbacks.filter(feedback => feedback.id !== id))
+
+    // Function for adding a new feedback from a completed form
+    const addFeedback = (newFeedback) => {
+        // uuidv4() as a unique id to the user created feedbacks
+        newFeedback.id = uuidv4()
+        setFeedbacks([ newFeedback, ...feedbacks])
     }
-    
-  }
 
-  return (
-    <>
-      <Header />
-      <div className="container">
-        <FeedbackForm />
-        <FeedbackStats data={feedbacks} />
-        <FeedbackList  data={feedbacks}
-        handleDelete={deleteFeedback} />
-      </div>
-    </>
-  )
+
+    // function to delete feedback/ratings. We'll have to 'prop-drill' all the way to FeedbackItem
+    const deleteFeedback = (id) => {
+        // Creating a warning popup to confirm delete action
+        if (window.confirm('Do you want to delete this feedback forever?')) {
+        setFeedbacks(feedbacks.filter(feedback => feedback.id !== id))
+        }
+        
+    }
+
+
+
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats data={feedbacks} />
+                <FeedbackList  data={feedbacks} handleDelete={deleteFeedback} />
+            </div>
+        </>
+    )
 }
 
 export default App 
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,44 +4,62 @@ import { useState } from 'react'
 import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
 
-function FeedbackForm() {
-  // Creating a default state for our form
-  const [texts, setTexts] = useState('')
-  const [rating, setRating] = useState(10)
-  const [btnDisabled, setBtnDisabled] = useState(true)
-  const [message, setMessage] = useState('')
+    function FeedbackForm({ handleAdd }) {
 
-  // This function allows up to capture the changes in our input field and set it as the new state()
-  const handleTextChange = (e) => {
-    
-    // Validation for a minimum of 15 characters before being able to submit a rating
-    if (texts === '') {
-      setBtnDisabled(true)
-      setMessage(null)
-    } else if (texts !== '' && texts.trim().length <= 15) {
-      setMessage('A Review must be a minimum of 15 characters')
-      setBtnDisabled(true)
-    } else {
-      setMessage(null)
-      setBtnDisabled(false)
+        // Creating a default state for our form
+        const [texts, setTexts] = useState('')
+        const [rating, setRating] = useState(10)
+        const [btnDisabled, setBtnDisabled] = useState(true)
+        const [message, setMessage] = useState('')
+
+        // This function allows up to capture the changes in our input field and set it as the new state()
+        const handleTextChange = (e) => {
+            
+            // Validation for a minimum of 15 characters before being able to submit a rating
+            if (texts === '') {
+                setBtnDisabled(true)
+                setMessage(null)
+            } else if (texts !== '' && texts.trim().length <= 15) {
+                setMessage('A Review must be a minimum of 15 characters')
+                setBtnDisabled(true)
+            } else {
+                setMessage(null)
+                setBtnDisabled(false)
+            }
+
+            setTexts(() => e.target.value)
+        }
+
+
+        // Function to handle the submission of a completed feedback form
+        const handleSubmit = (e) => {
+            e.preventDefault()
+            if(texts.trim().length > 15) {
+                const newFeedback = {
+                    text: texts,
+                    rating,
+                }
+            handleAdd(newFeedback)
+
+            // To clear textfield after submission
+            setTexts('')
+            }
+        }
+
+
+        return (
+            <Card>
+                <form onSubmit={handleSubmit}>
+                    <h2>Please let us know how you how we can better service you.</h2>
+                    <RatingSelect select={(rating) => setRating(rating)} />
+                    <div className="input-group">
+                        <input type="text" placeholder='Leave a review...' onChange={handleTextChange} value={texts} />
+                        <Button type='submit' isDisabled={btnDisabled} >Submit</Button>
+                    </div>
+                    {message && <div className='message'>{message}</div>}
+                </form>
+            </Card>
+        )
     }
 
-    setTexts(() => e.target.value)
-  }
-
-  return (
-    <Card>
-      <form>
-        <h2>Please let us know how you how we can better service you.</h2>
-        <RatingSelect select={(rating) => setRating(rating)} />
-        <div className="input-group">
-          <input type="text" placeholder='Leave a review...' onChange={handleTextChange} value={texts} />
-          <Button type='submit' isDisabled={btnDisabled} >Submit</Button>
-        </div>
-        {message && <div className='message'>{message}</div>}
-      </form>
-    </Card>
-  )
-}
-
-export default FeedbackForm
+    export default FeedbackForm
